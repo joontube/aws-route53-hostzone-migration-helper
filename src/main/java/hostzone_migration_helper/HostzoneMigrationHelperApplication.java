bc1724336980cdebcac6dc268006246e7083d40e
joontube <iglu123@naver.com>
2024-12-27 17:30:02 +0900

    INIT: Create Project Structure

---
+++ src/main/java/hostzone_migration_helper/HostzoneMigrationHelperApplication.java
@@ -0,0 +1,13 @@
+package hostzone_migration_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostzoneMigrationHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostzoneMigrationHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hostzone_migration_helper/HostzoneMigrationHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package hostzone_migration_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostzoneMigrationHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
